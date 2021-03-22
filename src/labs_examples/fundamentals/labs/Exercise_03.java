ce3f44dd70e5e8cb95e482e3c5dac611a6b12763
ncummons <cummonsn@gmail.com>
2021-03-22 12:39:20 -0400

    Removed Integer Example

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -16,11 +16,6 @@ public class Exercise_03 {
         System.out.println("Hello World!");
         // write code to print out "Check out my first program!" below
         System.out.println("Check out my first program!");
-        int i = 10;
-        int x = 12;
-        int z = i + x;
-
-        System.out.println(z);
     }
 
 }
