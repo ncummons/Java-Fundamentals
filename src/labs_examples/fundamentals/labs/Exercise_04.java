fdc21b1eb2f8bfd9d60fa83c6e925a6bc7bb28df
ncummons <cummonsn@gmail.com>
2021-03-23 22:54:30 -0400

    Exercise 3 & 4 Complete

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,29 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        boolean isTrue = true;
+        System.out.println("isTrue = " + isTrue);
 
-        // write your code below
+        char letter = 'G';
+        System.out.println("char = " + letter);
+
+        byte SmallNumber = 12;
+        System.out.println("byte = " + SmallNumber);
+
+        short MediumNumber = 15000;
+        System.out.println("MediumNumber = " + MediumNumber);
+
+        int MaxCashStack = 2147483647;
+        System.out.println("I now understand that in OSRS the max cash stack is: " + MaxCashStack + " because it is a Java-based game and they use an int value for the coin system. Neat.");
+
+        long BigNumber = 500000000;
+        System.out.println("BigNumber = " + BigNumber);
+
+        float DecimalNumber = 12.1845f;
+        System.out.println("DecimalNumber = " + DecimalNumber);
+
+        double PreciseDecimal = 12.18449375618;
+        System.out.println("PreciseDecimal = " + PreciseDecimal);
 
     }
 
