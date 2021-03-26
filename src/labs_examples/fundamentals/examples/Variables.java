b50915a76bbaf33aa3247ad8aec5e5a2d8eb10fd
ncummons <cummonsn@gmail.com>
2021-03-25 20:34:11 -0400

    Edited Variables example and Person example

--- src/labs_examples/fundamentals/examples/Variables.java
+++ src/labs_examples/fundamentals/examples/Variables.java
@@ -7,10 +7,32 @@ public class Variables {
     static double val = 123.45;
 
     public static void main(String[] args) {
-        double x = multiply(val,val * 2);
+
+        Person myPerson = new Person("Nicholas", 26);
+        Person yourPerson = new Person("Ryan", 36);
+        Person einstein = new Person("Einstein", 110);
+        Person ellen = new Person("Ellen", 50);
+
+        System.out.println(myPerson.toString());
+        System.out.println(yourPerson.toString());
+        System.out.println(einstein.toString());
+        System.out.println(ellen.toString());
+
+        double x = multiply(val,-2 * 2);
     }
     public static double multiply(double a, double b){
         double result = a * b;
+
+        if (result > 0){
+            double testVal = result * 2;
+            System.out.println(testVal * val);
+        }
+
+        if (result <= 0){
+            double testVal2 = result / 2;
+            System.out.println(testVal2 / val);
+        }
+
         printNum(result);
         return result;
     }
