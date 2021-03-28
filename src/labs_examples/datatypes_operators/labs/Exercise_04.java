9c5c7c7ef2c299eee8414c81e00786304f66c2ea
ncummons <cummonsn@gmail.com>
2021-03-27 21:45:31 -0400

    Exercise 4: data types and operators done

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -18,7 +18,40 @@ class RelationalOperators {
         }
 
         // write your code below
+        int x = 10;
+        int y = 10;
+        System.out.println("x is equal to: " + x + "\n" + "y is equal to: " + y);
 
+        if (x == y){
+            System.out.println("x is equal to y");
+        }
+        x -= 2;
+        System.out.println("x is now equal to: " + x);
+        if (x < y){
+            System.out.println("x is now less than y");
+        }
+        if (x <= y){
+            System.out.println("x is also less than or equal to y");
+        }
+        x += 2;
+        System.out.println("x is now equal to: " + x);
+
+        if (x <= y) {
+            System.out.println("x is still less than or equal to y");
+        }
+
+        if (x >= y){
+            System.out.println("x is also greater than or equal to y");
+        }
+        x -= 2;
+        System.out.println("x is now equal to: " + x);
+        if (!(x > y)){
+            System.out.println("However, x is not greater than y");
+        }
+
+        if (x != y){
+            System.out.println("Put quite simply, x is not equal to y");
+        }
     }
 
 }
