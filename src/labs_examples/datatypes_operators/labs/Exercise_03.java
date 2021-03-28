75deab3575eaff478cf1acfe9b1fb3c09479c91e
ncummons <cummonsn@gmail.com>
2021-03-27 21:38:11 -0400

    Exercise 3: data types and operators done

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,7 +11,18 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int x = 10;
+        int y = 3;
+        System.out.println("x = " + x + "\n" + "y = " + y);
 
+        int addition = x + y;
+        System.out.println("x + y = " + addition);
+        int subtraction = x - y;
+        System.out.println("x - y = " + subtraction);
+        int multiplication = x * y;
+        System.out.println("x * y = " + multiplication);
+        int division = x / y;
+        int modulus = x % y;
+        System.out.println("x / y = " + division + " with remainder " + modulus);
     }
-
 }
