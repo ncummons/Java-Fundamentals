c226fd51beb1c8e83b219b569ed79f54eab3cdcf
ncummons <cummonsn@gmail.com>
2021-03-29 12:22:52 -0400

    Conditional Statements: If/else and nested examples

---
+++ src/labs_examples/conditions_loops/examples/conditional_statements/ConditionalStatements.java
@@ -0,0 +1,33 @@
+package labs_examples.conditions_loops.examples.conditional_statements;
+
+public class ConditionalStatements {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        if (a < b){
+            //if statement code block
+            System.out.println("a is less than b");
+        } else if (b > 30) {
+            System.out.println("a greater than 30");
+        } else if (b > 20) {
+            System.out.println("a greater than 20");
+        } else if (b > 15) {
+
+                if (b == 20){
+                    System.out.println("b is 20");
+                }
+                else {
+                    System.out.println("a greater than 15");
+                }
+        } else {
+            System.out.println("a is not less than b and b is not greater than 30");
+        }
+
+    }
+
+}
