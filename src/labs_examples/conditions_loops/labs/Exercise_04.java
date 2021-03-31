37810af1f33399ab13e8e09121de43b4cf61ab49
ncummons <cummonsn@gmail.com>
2021-03-31 10:59:33 -0400

    Conditions_loops Exercises 3 and 4

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -9,4 +9,19 @@ package labs_examples.conditions_loops.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        int count;
+
+        for(count = 1; count <= 100; count++){
+            if(count % 2 == 0){
+                System.out.println(count);
+            }
+            else{
+                continue;
+            }
+        }
+
+    }
+
 }
