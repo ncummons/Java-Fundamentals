967569f78fe878f350da8e5e25186543560120f1
ncummons <cummonsn@gmail.com>
2021-04-06 15:11:47 -0400

    Arrays Exercises 6 and 7 done

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,5 @@
 package labs_examples.arrays.labs;
-
+import java.util.ArrayList;
 /**
  *  ArrayLists
  *
@@ -10,4 +10,58 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> testList = new ArrayList();
+
+        testList.add("New");
+        testList.add("list");
+        testList.add("of");
+        testList.add("strings.");
+
+        for(String words: testList){
+            System.out.print(words + " ");
+        }
+        System.out.println();
+
+        testList.add(1, "(now even more new)");
+
+        for(String words: testList){
+            System.out.print(words + " ");
+        }
+        System.out.println();
+
+        testList.set(1, "new");
+        testList.add("There, that should be less wordy... right?");
+
+        for(String words: testList){
+            System.out.print(words + " ");
+        }
+        System.out.println();
+
+        System.out.println("No, that is just awkward. Let's remove that.");
+
+        testList.remove(1);
+
+        for(String words: testList){
+            System.out.print(words + " ");
+        }
+        System.out.println();
+
+        String word = testList.get(1);
+        System.out.println("The second element in the test list is: " + word);
+        System.out.println("The length of the test list is: " + testList.size() + " elements.");
+
+        testList.clear();
+
+        if(testList.isEmpty()){
+            System.out.println("Now the test list is empty...");
+        }
+        else{
+            System.out.println("The test list is not empty.");
+        }
+
+    }
+
 }
