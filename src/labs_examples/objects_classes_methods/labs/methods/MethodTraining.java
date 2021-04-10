35852110925470ac375d7817af9aa02c3c7df1db
ncummons <cummonsn@gmail.com>
2021-04-10 14:11:18 -0400

    Methods Ex. 3 done :)

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -1,5 +1,7 @@
 package labs_examples.objects_classes_methods.labs.methods;
 
+import java.util.ArrayList;
+
 class MethodTraining {
 
     public static void main(String[] args) {
@@ -9,6 +11,7 @@ class MethodTraining {
         // My primitive data type, fakeAge will be "passed by value" and the method will receive a copy of the value
         // The actual integer, fakeAge is not changed -- just the value passed into the method
         System.out.println("Before running plus1: : " + fakeAge);
+        System.out.print("Running plus1: ");
         plus1(fakeAge);
         System.out.println("After: " + fakeAge);
 
@@ -23,13 +26,51 @@ class MethodTraining {
 
         // Print out the max value of 4 numbers:
 
-        System.out.println(maxValue(1235,4,56,4));
+        System.out.println(maxValue(1235, 4, 56, 4));
+
+        // Return an integer value for how many consonants are in a string
+
+        System.out.println("There are 20 Consonants in this string: " + countConsonants("There are 20 Consonants in this string"));
 
         // Return a boolean for a test of whether or not a number is prime:
 
         System.out.println("25 is prime: " + isPrime(25));
         System.out.println("13 is prime: " + isPrime(13));
         System.out.println("111 is prime: " + isPrime(111));
+
+        // Return a minimum and maximum value as an int array from a separate int array
+
+        int[] givenArray = {1, 2, 3, 4, 5, 6, 78, 8, -12, 10};
+        System.out.println("The min and mix of: 1, 2, 3, 4, 5, 6, 78, 8, -12, 10 are respectively: ");
+        int[] minMaxArr = minMax(givenArray);
+        for (int x = 0; x < minMaxArr.length; x++) {
+            System.out.print(minMaxArr[x] + " | ");
+        }
+        System.out.println();
+
+        // Returning int array list for nums divisible by both divisors b/w 0 and max num
+
+        ArrayList<Integer> newArrList = divisibleBy(100, 10, 5);
+        System.out.println("This new list has " + newArrList.size() + " elements.");
+        for (int vals : newArrList) {
+            System.out.print(vals + " | ");
+        }
+        System.out.println();
+
+        // Takes in an integer array and reverses it (one used one extra temp variable)
+
+        int[] valueArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.print("Here is the original array: ");
+        for (int intVals : valueArray) {
+            System.out.print(intVals + " | ");
+        }
+        System.out.println();
+        System.out.print("The new reversed array is: ");
+        valueArray = reverseIntArr(valueArray);
+        for (int intVals : valueArray) {
+            System.out.print(intVals + " | ");
+        }
+        System.out.println();
     }
 
     public static int sum(int a, int b) {
@@ -46,6 +87,7 @@ class MethodTraining {
         System.out.println(num);
     }
 
+    // Thought of this after, but I could have used a switch statement, right?
     public static int maxValue(int a, int b, int c, int d){
         if(a >= b & a >= c & a >= d){
             return a;
@@ -59,6 +101,22 @@ class MethodTraining {
         }
     }
 
+    public static int countConsonants(String string){
+        int sum = 0;
+        String consonants = "qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM";
+        char[] charArray = string.toCharArray();
+        for(int i = 0; i < charArray.length; i++){
+            for(int j = 0; j < consonants.length(); j++) {
+                if (charArray[i] == consonants.charAt(j)) {
+                    sum += 1;
+                }else{
+                    continue;
+                }
+            }
+        }
+        return sum;
+    }
+
     public static boolean isPrime(int x){
         for(int i = 2; i < x/2; i++){
             if(x % i == 0){
@@ -70,7 +128,42 @@ class MethodTraining {
         return true;
     }
 
+    public static int[] minMax(int[] intArr){
+        int maxValue = intArr[0];
+        int minValue = intArr[0];
+        for(int i = 1; i < intArr.length; i++){
+            if(intArr[i] > maxValue){
+                maxValue = intArr[i];
+            }
+        }
+        for(int i = 1; i < intArr.length; i++){
+            if(intArr[i] < minValue){
+                minValue = intArr[i];
+            }
+        }
+        int[] newArr = {minValue, maxValue};
+        return newArr;
+    }
 
+    public static ArrayList<Integer> divisibleBy(int maxNum, int divisor1, int divisor2){
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        for(int i = 1; i < maxNum; i++){
+            if((i % divisor1 == 0) & (i % divisor2 == 0)){
+                integerArrayList.add(i);
+            }
+        }
+        return integerArrayList;
+    }
+
+    public static int[] reverseIntArr(int[] intArr){
+        int temp;
+        for(int i = 0; i < intArr.length / 2; i++){
+            temp = intArr[i];
+            intArr[i] = intArr[intArr.length - 1 - i];
+            intArr[intArr.length - 1 - i] = temp;
+        }
+        return intArr;
+    }
 
 public static class Person {
 
