14eddd14e6df1967780ca11441a921ba03dd0232
ncummons <cummonsn@gmail.com>
2021-04-29 11:12:15 -0400

    I/O Exercises 3 and 4 done - relative paths

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,5 +1,9 @@
 package labs_examples.input_output.labs;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.*;
+
 /**
  * Input/Output Exercise 3: variety
  *
@@ -9,3 +13,108 @@ package labs_examples.input_output.labs;
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
  */
+
+class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        // 1.) Using two additional Byte Streams
+
+        String fromFilePath = "src/labs_examples/input_output/files/byte_data_2";
+        String toFilePath = "src/labs_examples/input_output/files/testFile";
+
+        try (FileInputStream fileInputStream = new FileInputStream(fromFilePath);
+             FileOutputStream fileOutputStream = new FileOutputStream(toFilePath)) {
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                fileOutputStream.write(c);
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // 2.) Using two additional Character Streams
+
+        fromFilePath = "src/labs_examples/input_output/files/char_data_2.txt";
+        toFilePath = "src/labs_examples/input_output/files/char_data_2_copy.txt";
+
+        try (FileReader fileReader = new FileReader(fromFilePath);
+        FileWriter fileWriter = new FileWriter(toFilePath)) {
+            int c;
+            while((c = fileReader.read()) != -1){
+                fileWriter.write(c);
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // 3.) Using a buffer on each
+
+        fromFilePath = "src/labs_examples/input_output/files/byte_data_2";
+        toFilePath = "src/labs_examples/input_output/files/testFile";
+
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(fromFilePath));
+             FileOutputStream fileOutputStream = new FileOutputStream(toFilePath)) {
+            int c;
+            while ((c = bufferedInputStream.read()) != -1) {
+                fileOutputStream.write(c);
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        fromFilePath = "src/labs_examples/input_output/files/char_data_2.txt";
+        toFilePath = "src/labs_examples/input_output/files/char_data_2_copy.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fromFilePath));
+             FileWriter fileWriter = new FileWriter(toFilePath)) {
+            int c;
+            while((c = bufferedReader.read()) != -1){
+                fileWriter.write(c);
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // 4.) Using the Data Streams
+
+        toFilePath = "src/labs_examples/input_output/files/data_stream_file";
+
+        try(DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(toFilePath))){
+            int x = 10;
+            double d = 15.6819;
+            boolean isTrue = true;
+
+            System.out.println("Writing: " + x);
+            dataOutputStream.writeInt(x);
+            System.out.println("Writing: " + d);
+            dataOutputStream.writeDouble(d);
+            System.out.println("Writing: " + isTrue);
+            dataOutputStream.writeBoolean(isTrue);
+
+
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+
+        fromFilePath = toFilePath;
+
+        try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream(fromFilePath))){
+            int x;
+            double d;
+            boolean isTrue;
+            x = dataInputStream.readInt();
+            System.out.println("Reading: " + x);
+            d = dataInputStream.readDouble();
+            System.out.println("Reading: " + d);
+            isTrue = dataInputStream.readBoolean();
+            System.out.println("Reading: " + isTrue);
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+
+        System.out.println("Completed program.");
+
+    }
+}
+
--- src/labs_examples/input_output/labs/Exercise_04.java
+++ src/labs_examples/input_output/labs/Exercise_04.java
@@ -1,5 +1,11 @@
 package labs_examples.input_output.labs;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  *      Following the video examples and source code found in the src/labs_examples/input_output/examples/csv_parser
@@ -12,3 +18,140 @@ package labs_examples.input_output.labs;
  *      Then, write the arraylist of objects back out to a new .csv file. Ensure that the resulting csv file is valid.
  *
  */
+
+class Exercise_04{
+
+    public static void main(String[] args) {
+
+        ArrayList<Guitar> guitars = new ArrayList<>();
+
+        String filePath = "src/labs_examples/input_output/files/guitarCSV.csv";
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+
+            String line;
+
+            while((line = bufferedReader.readLine()) != null){
+                String[] values = line.split(",");
+                guitars.add(mapValuesToGuitar(values));
+            }
+
+        }catch(FileNotFoundException fileNotFoundException){
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+
+        for(Guitar guitar : guitars){
+            System.out.println(guitar.toString());
+        }
+
+    }
+
+    private static Guitar mapValuesToGuitar(String[] values){
+        Guitar guitar = new Guitar();
+        guitar.setManufacturer(values[0]);
+        guitar.setModel(values[1]);
+        guitar.setNumFrets(Integer.parseInt(values[2]));
+        guitar.setNumStrings(Integer.parseInt(values[3]));
+        guitar.setDoubleNeck(Boolean.parseBoolean(values[4]));
+        guitar.setElectric(Boolean.parseBoolean(values[5]));
+        guitar.setRetailPrice(Double.parseDouble(values[6]));
+
+        return guitar;
+    }
+
+}
+
+class Guitar{
+
+    private String manufacturer;
+    private String model;
+    private int numFrets;
+    private int numStrings;
+    private boolean isDoubleNeck;
+    private boolean isElectric;
+    private double retailPrice;
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setNumFrets(int numFrets) {
+        this.numFrets = numFrets;
+    }
+
+    public void setNumStrings(int numStrings) {
+        this.numStrings = numStrings;
+    }
+
+    public void setDoubleNeck(boolean doubleNeck) {
+        isDoubleNeck = doubleNeck;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+
+    public Guitar() {
+    }
+
+    public Guitar(String manufacturer, String model, int numFrets, int numStrings, boolean isDoubleNeck, boolean isElectric, double retailPrice) {
+        this.setManufacturer(manufacturer);
+        this.setModel(model);
+        this.setNumFrets(numFrets);
+        this.setNumStrings(numStrings);
+        this.setDoubleNeck(isDoubleNeck);
+        this.setElectric(isElectric);
+        this.setRetailPrice(retailPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "Guitar{" +
+                "manufacturer='" + getManufacturer() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", numFrets=" + getNumFrets() +
+                ", numStrings=" + getNumStrings() +
+                ", isDoubleNeck=" + isDoubleNeck() +
+                ", isElectric=" + isElectric() +
+                ", retailPrice=" + getRetailPrice() +
+                '}';
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNumFrets() {
+        return numFrets;
+    }
+
+    public int getNumStrings() {
+        return numStrings;
+    }
+
+    public boolean isDoubleNeck() {
+        return isDoubleNeck;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+}
