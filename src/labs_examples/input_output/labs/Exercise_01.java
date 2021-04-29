bd05ac0ed6d44cfd812a4407daa1e1a6f3f8e028
ncummons <cummonsn@gmail.com>
2021-04-29 09:29:06 -0400

    I/O Exercises 1 and 2 done, pushing created files too.

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,49 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        String fromFilePath = "src/labs_examples/input_output/files/byte_data";
+        String toFilePath = "src/labs_examples/input_output/files/copy_byte_data";
 
+        FileInputStream fs = null;
+        BufferedInputStream bis = null;
+        FileOutputStream fo = null;
+        BufferedOutputStream bos = null;
+
+
+        try{
+            // This array holds and subsequently prints out the bytes that we are reading/writing so that we
+            // can verify that it is inputting/outputting 5 bytes each time
+            byte[] buffArr = new byte[5];
+            fs = new FileInputStream(fromFilePath);
+            bis = new BufferedInputStream(fs,5);
+            fo = new FileOutputStream(toFilePath);
+            bos = new BufferedOutputStream(fo,5);
+            int c;
+
+            while((c = bis.read(buffArr)) != -1) {
+                System.out.print(new String(buffArr, 0, c));
+                bos.write(c);
+            }
+
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        } finally {
+            try{
+                if(bis != null){
+                    bis.close();
+                }
+            }catch(IOException exc){
+                System.out.println("There was an error closing the input stream.");
+            } try{
+                if(bis != null){
+                    bos.close();
+                }
+            }catch(IOException exc){
+                System.out.println("There was an error closing the output stream.");
+            }
+
+        }
+        System.out.println("Done copying file.");
 
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,14 +1,63 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
  *      -Using the BufferedReader, read a file character by character and write an encrypted version to a new file.
  *      -For example, change every 'a' to '-' and every 'e' to '~' .
- *      -Make sure you close the connections to both files.
+ *      -Make sure you close the connections to both files. --> I used a "try-with-resources" to ensure that
+ *      the files were automatically closed and it is less verbose.
  *
- *      Then, ead back the encrypted file using the BufferedReader and
+ *      Then, read back the encrypted file using the BufferedReader and
  *      print out the unencrypted version. Does it match the original file?
  *
  */
+
+class Exercise_02{
+
+    // Read from file and write to new file with changes and then system output to console from the encrypted new file
+    // to verify that it is the same as the original file, reading through the changes.
+
+    public static void main(String[] args) {
+
+        String fromFilePath = "src/labs_examples/input_output/files/char_data.txt";
+        String toFilePath = "src/labs_examples/input_output/files/encrypted_char_data.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fromFilePath));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(toFilePath))){
+            int c;
+            while((c = br.read()) != -1){
+                if ((char) c == 'a') c = ')';
+                if ((char) c == 'e') c = '$';
+                if ((char) c == 'o') c = '|';
+                if ((char) c == 't') c = '>';
+                bw.write((int) c);
+            }
+
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+
+        // Here I will print out the text from the encrypted file to the console,
+        // changing the characters back to the originals
+
+        try(BufferedReader br = new BufferedReader(new FileReader(toFilePath))){
+            int c;
+            while((c = br.read()) != -1){
+                if ((char) c == ')') c = 'a';
+                if ((char) c == '$') c = 'e';
+                if ((char) c == '|') c = 'o';
+                if ((char) c == '>') c = 't';
+                System.out.print((char) c);
+            }
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+        System.out.println("Done reading, writing, and printing");
+
+    }
+
+}
 
