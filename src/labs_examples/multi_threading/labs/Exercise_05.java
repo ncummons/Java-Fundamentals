9b43d84ef27bf4fd8646200fab96bc84deae8550
ncummons <cummonsn@gmail.com>
2021-05-12 21:52:28 -0400

    MultiThreading Ex 5 Idea

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -4,4 +4,9 @@ package labs_examples.multi_threading.labs;
  * Multithreading Exercise 5:
  *
  *      Demonstrate the use of a wait() and notify()
+ *
+ *      Create sequence of threads, where second thread needs to wait until first thread has finished a specific
+ *      task such as summing an array, concatenating inputs, etc.
+ *
  */
+
