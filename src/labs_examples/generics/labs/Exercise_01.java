66a8be30c1f18f8167236f76485ac07c4373bbbb
ncummons <cummonsn@gmail.com>
2021-05-31 14:10:56 -0400

    Generics Ex 1 Complete

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,41 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+class MainControl{
+    public static void main(String[] args) {
+        GenericExample gen1 = new GenericExample("Awesome", "Cool");
+        System.out.println(gen1.getVal1());
+        System.out.println(gen1.getVal2());
+
+        GenericExample gen2 = new GenericExample("One", 1f);
+        System.out.println(gen2.getVal1());
+        System.out.println(gen2.getVal2());
+    }
+}
+
+class GenericExample <T, V>{
+
+    T val1;
+    V val2;
+
+    public GenericExample(T val1, V val2) {
+        this.val1 = val1;
+        this.val2 = val2;
+    }
+
+    public T getVal1() {
+        return val1;
+    }
+
+    public void setVal1(T val1) {
+        this.val1 = val1;
+    }
+
+    public V getVal2() {
+        return val2;
+    }
+
+    public void setVal2(V val2) {
+        this.val2 = val2;
+    }
+}
